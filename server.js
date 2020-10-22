e4ef8bac8e8024d8e4a0423f345429621e73fb27
preetgur <preetgur0137@gmail.com>
2020-10-22 20:16:07 +0530

    How to send html page using path in express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hello world l </h1>')
+    // res.send('<h1>Hello world l </h1>')
+    res.sendFile(path.join(__dirname,'public','index.html'));
 })
 
 const PORT = process.env.PORT || 5000
